e03923c8b82063779d3e993363526db043510266
Dominic Barajas <dominicbarajas548@gmail.com>
2021-11-18 13:04:01 -0700

    update spawn capabilities
    update the playerController to create a delay to the spawn  the dog prefrab to catch the ball. update to the spawnmanager to random.range the balls spawning on screen

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float timeToWait = 2;
+    public float timeCount = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > timeCount)
         {
+            timeCount = Time.time + timeToWait;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
+
 }
